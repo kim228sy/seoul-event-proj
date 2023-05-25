aac38f58f8dc316c4b40ddf2280ac79e94fe465e
kim228sy <kim228sy@gmail.com>
2023-05-25 17:13:15 +0900

    feat: 기초세팅

---
+++ app.js
@@ -0,0 +1,73 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+const cors = require("cors");
+const corsConfig = require("./config/corsConfig.json");
+// const logger = require('morgan'); // 구코드 삭제
+const logger = require("./lib/logger");
+const models = require("./models/index");
+const bodyParser = require("body-parser");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+logger.info("app start");
+const dotenv = require("dotenv");
+const NODE_ENV = process.env.NODE_ENV;
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(cors(corsConfig));
+// app.use(logger('dev')); // 구코드 삭제
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
+dotenv.config();
+
+models.sequelize
+  .authenticate()
+  .then(() => {
+    logger.info("DB connection success");
+
+    // sequelize sync (table 생성)
+    models.sequelize
+      .sync()
+      .then(() => {
+        logger.info("Sequelize sync success");
+      })
+      .catch((err) => {
+        logger.error("Sequelize sync error", err);
+      });
+  })
+  .catch((err) => {
+    logger.error("DB Connection fail", err);
+  });
